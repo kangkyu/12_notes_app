02e8e5d3119d41bf25f375bb7b29ddd7e199ad5f
Jen Diamond <thejendiamond@gmail.com>
2014-12-08 23:44:19 -0800

    further exploration with yaml

--- myapp.rb
+++ myapp.rb
@@ -1,18 +1,21 @@
 require 'yaml'
 
-readme = YAML::load(File.open('config.yml'))
-
+readme = YAML.load(File.open('config.yml'))
+puts readme.inspect
+# readme is a yaml method
 google = readme['google']
+google_line = google.each { |goog| puts goog }
+
 definition = readme['definition']
+def_sort = definition.sort
 
-
-File.open('google.txt', 'w') do |f|
-  f.write(google[0]) /n
-  f.write(google[2]) /n
-  f.write(google
+File.open('google.txt', 'a') do |f|
+  puts google
+  f.write(google)
+  # f.write(google[2])
+  #f.write(google[3])
 end
 
-File.open('definition.txt', 'w') do |f|
-  f.write(definition)
+File.open('definition.txt', 'a') do |f|
+  f.write(def_sort)
 end
-
